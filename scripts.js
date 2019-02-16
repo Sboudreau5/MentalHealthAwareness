29d21fcf3cc1d7d5158ec95f25d0d28bd3a443f9
Sboudreau5 <46534718+Sboudreau5@users.noreply.github.com>
2019-02-16 17:01:53 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,60 @@
+document.getElementById('depression').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal').style.display = 'flex'							   
+});
+
+
+document.querySelector('.close').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal').style.display = 'none'							   
+});
+
+
+
+document.getElementById('anxiety').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal2').style.display = 'flex'							   
+});
+
+
+document.querySelector('.close2').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal2').style.display = 'none'							   
+});
+
+
+document.getElementById('OCD').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal3').style.display = 'flex'							   
+});
+
+
+document.querySelector('.close3').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal3').style.display = 'none'							   
+});
+
+document.getElementById('PTSD').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal4').style.display = 'flex'							   
+});
+
+
+document.querySelector('.close4').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal4').style.display = 'none'							   
+});
+
+document.getElementById('questionmark').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal5').style.display = 'flex'							   
+});
+
+
+document.querySelector('.close5').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal5').style.display = 'none'							   
+});
+
+
+
